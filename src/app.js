a7de61c5146d195fbf013582803747439e4b2b3d
Your Name <you@example.com>
2023-02-02 20:18:14 +0100

    Típusok átnézve

---
+++ src/app.js
@@ -0,0 +1,24 @@
+var szam = '35';
+var szam2 = '27';
+console.log(Number(szam) + Number(szam2));
+
+var esik = true;
+if (esik) {
+    console.log('esik...')
+}
+
+var calcArea = function(base, height) {
+    return base * height /2;
+}
+console.log(typeof calcArea)
+
+var pista = {
+    nev: 'Pisti',
+    kor: 28,
+    van_kocsi: true
+};
+
+console.log(typeof pista)
+
+var szamok = [25, 30, 40];
+console.log(typeof szamok);
